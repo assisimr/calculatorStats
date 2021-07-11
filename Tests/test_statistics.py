7eac31b82a11d2eb05d23e1d0f8b302dae5533ec
simran <ska7@njit.edu>
2021-07-11 12:32:43 -0400

    Task: Added Calculator and Statistics Tests

---
+++ Tests/test_statistics.py
@@ -0,0 +1,19 @@
+import unittest
+from Statistics.statistics import Statistics
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testData = (2, 4, 6, 8)
+        self.statistics = Statistics()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+    def test_mean_calculator(self):
+        mean = self.statistics.mean(self.testData)
+        self.assertEqual(mean, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
