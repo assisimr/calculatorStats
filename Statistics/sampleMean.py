d10e3b25b47880931dce4c0cf5e58f715a977674
simran <ska7@njit.edu>
2021-07-12 14:36:55 -0400

    Task: Ran tests for mean

--- Statistics/sampleMean.py
+++ Statistics/sampleMean.py
@@ -11,3 +11,12 @@ def sample_mean(data, sample_size):
     for num in sample:
         total = addition(total, num)
     return division(total, num_values)
+
+def sample_median(data, sample_size):
+    total = 0
+
+    sample = getSample(data, sample_size)
+    num_values = len(sample)
+    for num in sample:
+        total = addition(total, num)
+    return division(total, num_values)
--- Statistics/statistics.py
+++ Statistics/statistics.py
@@ -1,10 +1,15 @@
 from Calculator.Calculator import Calculator
 from Statistics.mean import mean
+from Statistics.median import median
 
 
 class Statistics(Calculator):
 
     def mean(self, data):
         self.result = mean(data)
+        return self.result
+
+    def median(self, data):
+        self.result = median(data)
         return self.result
 
